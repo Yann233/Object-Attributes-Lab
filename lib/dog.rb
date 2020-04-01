68c4848b1d898decfb4c0773365b1857916692af
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-01 13:43:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,6 @@
 class Dog
+#Teach Dog about their names through two methods, #name, and #name= 
+#that read and write to a corresponding instance variable @name.
     def name=(dog_name)
         @name = dog_name
     end
